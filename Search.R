8484c5de649489e0e782679cf1ab7a67f69f25e6
Akiko Iwamizu <akikoiwamizu@gmail.com>
2015-08-25 12:10:43 -0700

    Updated code 08/25

--- Search.R
+++ Search.R
@@ -9,22 +9,55 @@ raw_data=read.csv("/Users/aiwamizu/Desktop/Work/Projects/Search Integration/SEAR
 ##########################
 
 #Remove columns to include ONLY the ones that are necessary
-keeps <- c("EVENT_DATE_KEY", "TSN", "SEARCH_STARTING_TIMESTAMP", "SEARCH_TEXT", "UI_VALUE_DESC", "SEARCH_CURRENT_TIMESTAMP", "SEARCH_EVENT_TYPE", "DISPLAY_NAME", "SEARCH_RESULT_CATEGORY")
+keeps <- c("EVENT_DATE_KEY", "TSN", "SEARCH_TEXT", "UI_VALUE_DESC", "SEARCH_RESULT_CATEGORY")
 clean_data <- raw_data[ , keeps]
 
 #Subset only successful searches
-new_data <- clean_data[which(clean_data$SEARCH_RESULT_CATEGORY == 'SUCCESS'), ]
-new_data$UI_VALUE_DESC <- strsplit(new_data$UI_VALUE_DESC,' ',fixed=TRUE)
+successful_searches <- clean_data[which(clean_data$SEARCH_RESULT_CATEGORY == 'SUCCESS'), ]
+successful_searches$SEARCH_RESULT_CATEGORY <- NULL
+successful_searches$SEARCH_TEXT <- tolower(successful_searches$SEARCH_TEXT)
+successful_searches$UI_VALUE_DESC <- tolower(successful_searches$UI_VALUE_DESC)
+successful_searches$SEARCH_TEXT <- strsplit(as.character(successful_searches$SEARCH_TEXT),' ', fixed=TRUE)
+#successful_searches$UI_VALUE_DESC <- strsplit(as.character(successful_searches$UI_VALUE_DESC),' ', fixed=TRUE)
 
 #Search Title or Contextual?
-#new_data$SEARCH_TEXT <- tolower(new_data$SEARCH_TEXT)
-#new_data$UI_VALUE_DESC <- tolower(new_data$UI_VALUE_DESC)
-new_data$SEARCH_CONTEXT <- agrepl(new_data$SEARCH_TEXT, new_data$UI_VALUE_DESC, ignore.case = TRUE)
-  #grepl(new_data$SEARCH_TEXT, new_data$UI_VALUE_DESC, fixed = TRUE)
-  #ifelse(new_data$SEARCH_TEXT %in% new_data$UI_VALUE_DESC, TRUE, FALSE)
+
+#Splitting search result into separate columns in order to compare the search text to each word in the result
+library(splitstackshape)
+expanded_search_list <- cSplit(successful_searches, "UI_VALUE_DESC", sep = " ", "wide")
+
+#Attempting a match up between search text and search result
+expanded_search_list$SEARCH_CONTEXT <- (grepl(expanded_search_list$SEARCH_TEXT, expanded_search_list$UI_VALUE_DESC_01, ignore.case = TRUE, fixed = FALSE)
+                                               | grepl(expanded_search_list$SEARCH_TEXT, expanded_search_list$UI_VALUE_DESC_02, ignore.case = TRUE, fixed = FALSE)
+                                               | grepl(expanded_search_list$SEARCH_TEXT, expanded_search_list$UI_VALUE_DESC_03, ignore.case = TRUE, fixed = FALSE)
+                                               | grepl(expanded_search_list$SEARCH_TEXT, expanded_search_list$UI_VALUE_DESC_04, ignore.case = TRUE, fixed = FALSE))
+                                              
+                                              
+#Manually fix the negative false results
+neg_false <- subset(expanded_search_list, expanded_search_list$SEARCH_CONTEXT == FALSE)
+neg_false$SEARCH_CONTEXT <- (pmatch(neg_false$SEARCH_TEXT, neg_false$UI_VALUE_DESC_01, nomatch = FALSE, duplicates.ok = TRUE)
+                             | pmatch(neg_false$SEARCH_TEXT, neg_false$UI_VALUE_DESC_02, nomatch = FALSE, duplicates.ok = TRUE)
+                             | pmatch(neg_false$SEARCH_TEXT, neg_false$UI_VALUE_DESC_03, nomatch = FALSE, duplicates.ok = TRUE)
+                             | pmatch(neg_false$SEARCH_TEXT, neg_false$UI_VALUE_DESC_04, nomatch = FALSE, duplicates.ok = TRUE)
+                             | pmatch(neg_false$SEARCH_TEXT, neg_false$UI_VALUE_DESC_05, nomatch = FALSE, duplicates.ok = TRUE)
+                             | grepl(neg_false$SEARCH_TEXT, neg_false$UI_VALUE_DESC_01, ignore.case = TRUE, fixed = FALSE)
+                             | grepl(neg_false$SEARCH_TEXT, neg_false$UI_VALUE_DESC_02, ignore.case = TRUE, fixed = FALSE)
+                             | grepl(neg_false$SEARCH_TEXT, neg_false$UI_VALUE_DESC_03, ignore.case = TRUE, fixed = FALSE)
+                             | grepl(neg_false$SEARCH_TEXT, neg_false$UI_VALUE_DESC_04, ignore.case = TRUE, fixed = FALSE)
+                             | grepl(neg_false$SEARCH_TEXT, neg_false$UI_VALUE_DESC_05, ignore.case = TRUE, fixed = FALSE)
+                             | (neg_false$SEARCH_TEXT %in% neg_false$UI_VALUE_DESC_01)
+                             | (neg_false$SEARCH_TEXT %in% neg_false$UI_VALUE_DESC_02)
+                             | (neg_false$SEARCH_TEXT %in% neg_false$UI_VALUE_DESC_03)
+                             | (neg_false$SEARCH_TEXT %in% neg_false$UI_VALUE_DESC_04)
+                             | (neg_false$SEARCH_TEXT %in% neg_false$UI_VALUE_DESC_05)
+                             )
 
 
 #Proportion count
-table(new_data$SEARCH_CONTEXT)
+table(expanded_search_list$SEARCH_CONTEXT)
+table(neg_false$SEARCH_CONTEXT)
 
-
+#Sum the number of title and contextual searches & find the percentages of total successful searches
+title_searches <- sum(length(which(expanded_search_list$SEARCH_CONTEXT == TRUE)), length(which(neg_false$SEARCH_CONTEXT == TRUE)), na.rm = TRUE)
+(title_searches/106721) * 100
+((106721 - title_searches)/106721) * 100
